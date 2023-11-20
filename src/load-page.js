b3f7e81b1836eddd9d66473631b67a4f6909f36f
Catherine Crenshaw <55335367+ccrenshawcodes@users.noreply.github.com>
2023-11-20 10:29:49 -0700

    Rearrange order of operations
    Populate sidebar with projects before loading other things

--- src/load-page.js
+++ src/load-page.js
@@ -4,14 +4,13 @@ import { showNewProjectModalOnClick } from './new-project-modal.js';
 import { getAndPopulateProjects } from './populate-sidebar.js';
 
 function loadPage () {
+    getAndPopulateProjects();
     const newTaskButton = document.querySelector('.new-task');
     const newProjectButton = document.querySelector('.new-project');
     showTaskModalOnClick(newTaskButton);
     addButtonFunctions();
     showNewProjectModalOnClick(newProjectButton);
-    getAndPopulateProjects();
     listAllItems();
-
 }
 
 export {
