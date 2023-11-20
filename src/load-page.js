f4389b0f612171f4507479c6d38ef70e999c566f
Catherine Crenshaw <55335367+ccrenshawcodes@users.noreply.github.com>
2023-11-20 08:48:35 -0700

    Start localStorage earlier in the page load process
    Hopefully this will fix our "reload page to see
    content" bug

--- src/load-page.js
+++ src/load-page.js
@@ -5,6 +5,7 @@ import { showNewProjectModalOnClick } from './new-project-modal.js';
 import { getAndPopulateProjects } from './populate-sidebar.js';
 
 function loadPage () {
+    startStorage();
     const newTaskButton = document.querySelector('.new-task');
     const newProjectButton = document.querySelector('.new-project');
     showTaskModalOnClick(newTaskButton);
@@ -13,7 +14,6 @@ function loadPage () {
     showNewProjectModalOnClick(newProjectButton);
 
     getAndPopulateProjects();
-    startStorage();
     listAllItems();
 
 }
